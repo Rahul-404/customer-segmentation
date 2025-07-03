c25ea9cb625506f3b66f802883b0112abf4032a2
Rahul-404 <rahulshelke981@gmail.com>
2025-07-04 00:37:34 +0530

    feat: Implement prediction pipeline and data processing methods for customer segmentation

--- customer_segmentation/pipeline/train_pipeline.py
+++ customer_segmentation/pipeline/train_pipeline.py
@@ -1,51 +1,64 @@
+import sys
 from typing import Tuple
+
 from pandas import DataFrame
+
+
+from customer_segmentation.components.data_ingestion import DataIngestion
+from customer_segmentation.components.data_transformation import DataTransformation
+from customer_segmentation.components.data_validation import DataValidation
+from customer_segmentation.components.model_trainer import ModelTrainer
+from customer_segmentation.components.model_evaluation import ModelEvaluation
+
+from customer_segmentation.components.model_pusher import ModelPusher
 
 from customer_segmentation.exception import CustomerException
 from customer_segmentation.logger import logging
-import os, sys
-
-# input for function
-from customer_segmentation.entity.config_entity import (DataIngestionConfig,
-                                                        DataValidationConfig)
-
-# function
-from customer_segmentation.component.data_ingestion import DataIngestion
-from customer_segmentation.component.data_validation import DataValidation
-
-# output for function
 from customer_segmentation.entity.artifact_entity import (DataIngestionArtifact,
-                                                          DataValidationArtifact)
+                                           DataTransformationArtifact,
+                                           DataValidationArtifact,
+                                           ModelEvaluationArtifact,
+                                           ModelTrainerArtifact)
+from customer_segmentation.entity.config_entity import (DataIngestionConfig,
+                                         DataTransformationConfig,
+                                         DataValidationConfig,
+                                         ModelEvaluationConfig,
+                                         ModelPusherConfig, ModelTrainerConfig)
+
 
 
 class TrainPipeline:
-
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-
+        self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
+        
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
-
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
 
         try:
             logging.info("Getting the data from mongodb")
 
-            data_ingestion = DataIngestion(data_ingestion_config = self.data_ingestion_config)
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            
 
-            logging.info("Got the train_set and train_set from mongodb")
+            logging.info("Got the train_set and test_set from mongodb")
 
             logging.info(
-                "Executed the start_data_ingestion method of trainPipeline class"
+                "Exited the start_data_ingestion method of TrainPipeline class"
             )
 
             return data_ingestion_artifact
-        
+
         except Exception as e:
-            raise CustomerException(e, sys)
-        
+            raise CustomerException(e, sys) from e
+
+    
     def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact) -> DataValidationArtifact:
         logging.info("Entered the start_data_validation method of TrainPipeline class")
 
@@ -67,7 +80,77 @@ class TrainPipeline:
 
         except Exception as e:
             raise CustomerException(e, sys) from e
+
+    
+    def start_data_transformation(self,
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        logging.info(
+            "Entered the start_data_transformation method of TrainPipeline class"
+        )
+
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact,
+                data_tranasformation_config=self.data_transformation_config
+            )
+            
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+
+            
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
+
+    
+
+    
+    def start_model_trainer(self,data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        logging.info("Entered the start_model_trainer method of TrainPipeline class")
+
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise CustomerException(e, sys) from e
         
+    def start_model_evaluation(
+        self,
+        data_ingestion_artifact: DataIngestionArtifact,
+        model_trainer_artifact: ModelTrainerArtifact,
+        data_transformation_artifact: DataTransformationArtifact
+    ) -> ModelEvaluationArtifact:
+        try:
+            model_evaluation = ModelEvaluation(
+                model_eval_config=self.model_evaluation_config,
+                data_ingestion_artifact=data_ingestion_artifact,
+                model_trainer_artifact=model_trainer_artifact,
+                data_transformation_artifact= data_transformation_artifact
+            )
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
+        except Exception as e:
+            raise CustomerException(e, sys)
+
+    def start_model_pusher(self, model_trainer_artifact: ModelTrainerArtifact):
+        """
+        """
+        try:
+            model_pusher = ModelPusher(
+                model_trainer_artifact=model_trainer_artifact,
+                model_pusher_config=self.model_pusher_config,
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise CustomerException(e, sys)
+
     def run_pipeline(self) -> None:
         logging.info("Entered the run_pipeline method of TrainPipeline class")
 
@@ -76,6 +159,28 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact = data_ingestion_artifact
             )
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact
+            )
+            
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact= data_transformation_artifact)
+            logging.info("model trained successfully")
+            
+            
+            model_evaluation_artifact = self.start_model_evaluation(
+            data_ingestion_artifact=data_ingestion_artifact,
+            model_trainer_artifact=model_trainer_artifact,
+            data_transformation_artifact= data_transformation_artifact
+            )
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(
+                model_trainer_artifact=model_trainer_artifact
+            )
+
+            logging.info("Exited the run_pipeline method of TrainPipeline class")
 
         except Exception as e:
             raise CustomerException(e, sys)
